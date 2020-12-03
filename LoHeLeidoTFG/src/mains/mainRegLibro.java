0ff3bb20024050bc9224609f38d88811ad1ea12d
PabloAja1000 <superuloheleido@hotmail.com>
2020-12-03 20:49:08 +0100

    creacion de los.class

---
+++ LoHeLeidoTFG/src/mains/mainRegLibro.java
@@ -0,0 +1,10 @@
+package mains;
+
+import Libreria.RegistrarLibro;
+
+public class mainRegLibro {
+  public static void main(String[] args) {
+    RegistrarLibro ventana = new RegistrarLibro();
+    ventana.iniciar();
+  }
+}
